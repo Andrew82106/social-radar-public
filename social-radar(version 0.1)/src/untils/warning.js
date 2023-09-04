8f4b116c2f4bbb8790c157bca3f08515a930ebc4
Andrew82106 <qq281512383@163.com>
2023-09-04 16:55:38 +0800

    modify readme

---
+++ social-radar(version 0.1)/src/untils/warning.js
@@ -0,0 +1,37 @@
+const warning={
+	"message": "请求成功",
+	"code": 200,
+	"success": true,
+	"data": {
+		"numPerPage": 10,
+		"totalRows": 4,
+		"totalPages": 1,
+		"currentPage": 1,
+		"resultList": [
+			{
+				"grade": "1",
+				"title": "深度伪造丑化领导人形象视频",
+				"id": "10086"
+			},
+			{
+			"grade": "1",
+			"title": "航空界机场集会",
+			"id": "10086"
+		}, {
+			"grade": "1",
+			"title": "8.5全港大罢工",
+			"id": "10086"
+		}, {
+			"grade": "1",
+			"title": "纽约反送中大游行",
+			"id": "10086"
+		},
+		{
+			"grade": "1",
+			"title": "港人中环“和你Lunch”",
+			"id": "10086"
+		},
+	]
+	}
+}
+export default warning
