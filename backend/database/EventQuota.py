26dd9a95ae9ba83b9e5598dd581d2eac012b6de8
Andrew82106 <qq281512383@163.com>
2023-11-21 11:32:52 +0800

    确定了指标的计算方法，主要指标都包含在QuotaCalculate里面了

--- backend/database/EventQuota.py
+++ backend/database/EventQuota.py
@@ -44,6 +44,9 @@ class EventQuota(BaseInfo):
              "quta6": 0.216,
              },
         ]
+        # 这里的ID_Index和platform_index分别指事件ID和平台名称
+        # 每一个事件暂定有6个指标，分别表示：
+        # 时间敏感度指标、内容敏感度指标、用户真实度指标、情感激烈性指标、观点对立性指标和热度持续性指标
 
     def fetch_detail(self, ID, ID2=None):
         if self.data is None:
