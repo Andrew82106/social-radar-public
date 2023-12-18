06e769b22074432d6c6794d1cdda8e684396fae3
Chris White <chriswhite33333@gmail.com>
2023-12-18 17:40:57 +0800

    Update platform selection and active platform state

--- frontend/src/components/hooks/EventIdContext.js
+++ frontend/src/components/hooks/EventIdContext.js
@@ -4,11 +4,12 @@ const EventIdContext = createContext();
 
 export function EventIdProvider({ children }) {
   const [eventId, setEventId] = useState(1);
-  const [platform, setPlatform] = useState([]);
+  const [platform, setPlatform] = useState([ "bilibili", "zhihu", "wangyi" ]);
+  const [activePlatform, setActivePlatform] = useState("bilibili");
 
   return (
     <EventIdContext.Provider
-      value={{ eventId, setEventId, platform, setPlatform }}
+      value={{ eventId, setEventId, platform, setPlatform, activePlatform, setActivePlatform }}
     >
       {children}
     </EventIdContext.Provider>
